ddf6858062db4f40ed0648b9ca16ecc9d84ba7ef
AnruthaKamal <107014168+AnruthaKamal@users.noreply.github.com>
2022-10-30 14:20:00 +0530

    Update fileupload.py

--- fileupload.py
+++ fileupload.py
@@ -4,7 +4,7 @@ def read_resume(byted_data,ext):
     if ext =='application/pdf':
         with open("temp.pdf", 'wb') as f:
             f.write(byted_data)
-        #text1 = textract.process("D:/MSc DATA SCIENCE/sem 5/ML project/temp.pdf")
+        #text1 = textract.process("temp.pdf")
         #text1 = text1.decode()
         pdfFileObj = open('temp.pdf', 'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
@@ -18,7 +18,7 @@ def read_resume(byted_data,ext):
     elif ext == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
         with open("temp.docx", 'wb') as f:
             f.write(byted_data)
-        text1 = textract.process("D:/MSc DATA SCIENCE/sem 5/ML project/temp.docx")
+        text1 = textract.process("temp.docx")
         text1 = text1.decode()
         return text1
 
